907f56898cb6a6d91932e221cf6667fc40ffe26a
ellydhore <ellydhoregabrylle.belleza@gmail.com>
2025-05-04 23:22:47 +0800

    minor fix

--- firebase_setup.py
+++ firebase_setup.py
@@ -4,6 +4,6 @@ from firebase_admin import credentials, firestore
 
 def initialize_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate(json.loads(st.secrets["firebase_json"]))       
+        cred = credentials.Certificate(st.secrets["firebase_json"])       
         firebase_admin.initialize_app(cred)
     return firestore.client()
